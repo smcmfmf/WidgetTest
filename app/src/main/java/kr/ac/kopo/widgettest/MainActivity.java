ea26be14378e041b431704298f09addbd6ae9051
smcmfmf <hk0831bae@gmail.com>
2025-03-24 16:25:24 +0900

    4장 기본 위젯의 속성들

--- app/src/main/java/kr/ac/kopo/widgettest/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/widgettest/MainActivity.java
@@ -1,6 +1,11 @@
 package kr.ac.kopo.widgettest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    RadioGroup rg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.enable_clickable_test); // 실행할 xml 파일 변경
+
+/*        rg = findViewById(R.id.rg);
+
+        Button btnDone = findViewById(R.id.btn_done);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = "";
+
+                if(rg.getCheckedRadioButtonId() == R.id.rb_male)
+                {
+                    result = getResources().getString(R.string.male);
+                }
+                else
+                {
+                    result = getResources().getString(R.string.female);
+                }
+
+                Toast toast = Toast.makeText(getApplicationContext(), result+"(을)를 선택하였습니다.", Toast.LENGTH_SHORT);
+                toast.show(); // 토스트 실행
+            }
+        });*/
     }
 }
